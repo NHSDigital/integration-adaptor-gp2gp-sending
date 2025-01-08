49ecd27ac8cf61515260b3ec62bf475ed8caa9ac
hospel <12736698+ole4ryb@users.noreply.github.com>
2025-01-08 10:06:48 +0000

    adjusting tests to satisfy more permutations

--- service/src/test/java/uk/nhs/adaptors/gp2gp/common/storage/S3StorageConnectorTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/storage/S3StorageConnectorTest.java
@@ -13,7 +13,6 @@ import java.io.ByteArrayInputStream;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -39,9 +38,9 @@ class S3StorageConnectorTest {
 
     @Test
     void expectExceptionWhenS3ClientCantDeliverResponse() {
-        s3StorageConnector = new S3StorageConnector(S3Client.builder().build(), config);
+        S3StorageConnector storageConnector = new S3StorageConnector(S3Client.builder().build(), config);
         Exception exception = assertThrows(StorageConnectorException.class,
-                                           () -> s3StorageConnector.downloadFromStorage("s3File"));
+                                           () -> storageConnector.downloadFromStorage("s3File"));
 
         assertEquals("Error occurred downloading from S3 Bucket", exception.getMessage());
     }
@@ -51,12 +50,13 @@ class S3StorageConnectorTest {
         var mockResponse = mock(GetObjectResponse.class);
         var mockInputStream = new ByteArrayInputStream("dummy-content".getBytes());
         var mockResponseInputStream = new ResponseInputStream<>(mockResponse, mockInputStream);
+        final var request = GetObjectRequest.builder().bucket(config.getContainerName()).key(FILE_NAME).build();
 
-        when(mockS3Client.getObject(any(GetObjectRequest.class))).thenReturn(mockResponseInputStream);
+        when(mockS3Client.getObject(request)).thenReturn(mockResponseInputStream);
 
         var result = s3StorageConnector.downloadFromStorage(FILE_NAME);
 
         assertNotNull(result);
-        verify(mockS3Client).getObject(any(GetObjectRequest.class));
+        verify(mockS3Client).getObject(request);
     }
 }
