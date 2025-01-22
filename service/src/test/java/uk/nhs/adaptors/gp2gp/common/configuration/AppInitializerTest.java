e25a07d3c69561e6255b814e58bda3fba52355af
hospel <12736698+ole4ryb@users.noreply.github.com>
2025-01-22 12:10:07 +0000

    increasing test coverage

---
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/configuration/AppInitializerTest.java
@@ -0,0 +1,52 @@
+package uk.nhs.adaptors.gp2gp.common.configuration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.s3.S3Client;
+import uk.nhs.adaptors.gp2gp.common.storage.StorageConnectorConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class AppInitializerTest {
+
+    private AppInitializer appInitializer;
+    private StorageConnectorConfiguration storageConnectorConfiguration;
+
+    @BeforeEach
+    void setUp() {
+        storageConnectorConfiguration = new StorageConnectorConfiguration();
+    }
+
+    @Test
+    void getS3ClientWhenTrustStoreUrlExists() {
+        storageConnectorConfiguration.setTrustStoreUrl("s3://localhost");
+        appInitializer = new AppInitializer(storageConnectorConfiguration);
+
+        S3Client s3Client = appInitializer.getS3Client();
+
+        assertNotNull(s3Client);
+    }
+
+    @Test
+    void getNullWhenTrustStoreUrlDoesNotExists() {
+
+        storageConnectorConfiguration.setTrustStoreUrl(null);
+        appInitializer = new AppInitializer(storageConnectorConfiguration);
+
+        S3Client s3Client = appInitializer.getS3Client();
+
+        assertNull(s3Client);
+    }
+
+    @Test
+    void getNullWhenTrustStoreUrlDoesNotStartWithS3Prefix() {
+
+        storageConnectorConfiguration.setTrustStoreUrl("http://localhost");
+        appInitializer = new AppInitializer(storageConnectorConfiguration);
+
+        S3Client s3Client = appInitializer.getS3Client();
+
+        assertNull(s3Client);
+    }
+}
