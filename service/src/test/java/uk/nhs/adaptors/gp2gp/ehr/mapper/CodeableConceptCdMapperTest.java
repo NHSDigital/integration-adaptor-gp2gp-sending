b19f33a577815aac89a70491d8402a65c7724dc8
MartinWheelerMT <martin.wheeler@madetech.com>
2025-03-05 16:11:05 +0000

    Remove now unnecessary test
    * Remove now unnecessary test

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/CodeableConceptCdMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/CodeableConceptCdMapperTest.java
@@ -169,51 +169,6 @@ public class CodeableConceptCdMapperTest {
         assertThat(outputXml).isEqualToIgnoringWhitespace(expectedOutputXML);
     }
 
-    @Test
-    void When_MappingStubbedCodeableConceptAsInactiveAllergy_Expect_NullFlavourCodeableConceptWithOriginalTextAsDisplayName() {
-        var inputJson = """
-            {
-                "resourceType": "AllergyIntolerance",
-                "id": "0C1232CF-D34B-4C16-A5F4-0F6461C51A41",
-                "meta": {
-                    "profile": [
-                        "https://fhir.nhs.uk/STU3/StructureDefinition/CareConnect-GPC-AllergyIntolerance-1"
-                    ]
-                },
-                "identifier": [
-                    {
-                        "system": "https://EMISWeb/A82038",
-                        "value": "55D2363D57A248F49A745B2E03F5E93D0C1232CFD34B4C16A5F40F6461C51A41"
-                    }
-                ],
-                "clinicalStatus": "inactive",
-                "code": {
-                    "coding": [
-                        {
-                            "system": "http://snomed.info/sct",
-                            "code": "292971006",
-                            "display": "Pivampicillin adverse reaction pt"
-                        }
-                    ]
-                }
-            }""";
-
-        var expectedOutputXML = """
-            <code nullFlavor="UNK">
-                <originalText>Pivampicillin adverse reaction pt</originalText>
-            </code>
-            """;
-        var codeableConcept = fhirParseService.parseResource(inputJson, AllergyIntolerance.class).getCode();
-
-        var outputXml = codeableConceptCdMapper.mapToNullFlavorCodeableConceptForAllergy(
-            codeableConcept,
-            AllergyIntolerance.AllergyIntoleranceClinicalStatus.INACTIVE
-        );
-
-        assertThat(outputXml).isEqualToIgnoringWhitespace(expectedOutputXML);
-
-    }
-
     @ParameterizedTest
     @MethodSource("getTestArgumentsActualProblem")
     public void When_MappingStubbedCodeableConceptForActualProblemHeader_Expect_HL7CdObjectXml(String inputJson, String outputXml)
