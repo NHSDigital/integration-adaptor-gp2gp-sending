db86f41c9a241075206be83a3c4541c4165209c4
MartinWheelerMT <88717465+MartinWheelerMT@users.noreply.github.com>
2025-06-20 09:32:21 +0100

    E2E Tests failing on GitHub build workflow (#1262)
    * Fix issue with E2E Test in Build Workflow where `docker compose build` was not finishing before attempting to `docker compose up`.  there also seemed to be a problem with the WireMock update, although this may now be fixed due to the workflow update.

--- docker/run-e2e-tests.sh
+++ docker/run-e2e-tests.sh
@@ -10,5 +10,5 @@ fi
 if [[ "$(docker network ls | grep "commonforgp2gp")" == "" ]] ; then
     docker network create commonforgp2gp
 fi
-docker-compose -f docker-compose.yml -f docker-compose-e2e-tests.yml build gp2gp-e2e-tests
-docker-compose -f docker-compose.yml -f docker-compose-e2e-tests.yml up --exit-code-from gp2gp-e2e-tests gp2gp-e2e-tests
+docker compose -f docker-compose.yml -f docker-compose-e2e-tests.yml build gp2gp-e2e-tests
+docker compose -f docker-compose.yml -f docker-compose-e2e-tests.yml up --exit-code-from gp2gp-e2e-tests gp2gp-e2e-tests
