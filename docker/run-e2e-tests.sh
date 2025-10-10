e06a52d74f5ccc2802cee05714a7aa1c712a0a2f
chiaramapellimt <chiara.mapelli@madetech.com>
2025-10-10 11:23:33 +0100

    Fixed config for running e2e

--- docker/run-e2e-tests.sh
+++ docker/run-e2e-tests.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ -f "vars.sh" ]; then
-    source vars.sh
+if [ -f "vars.local.e2e.sh" ]; then
+    source vars.local.e2e.sh
 else
   echo "No vars.sh defined. Using docker-compose defaults."
 fi
