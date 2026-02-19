b33e7b27db070d987cfa672566a7a3022f1c7370
Jon Hardy <Jonathan.Hardy97@gmail.com>
2026-02-19 10:22:33 +0000

    revert usage of data annotation and replace with getter and setter annotations

--- service/src/main/java/uk/nhs/adaptors/gp2gp/common/configuration/Gp2gpConfiguration.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/common/configuration/Gp2gpConfiguration.java
@@ -1,16 +1,17 @@
 package uk.nhs.adaptors.gp2gp.common.configuration;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Data
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "gp2gp")
 @EnableScheduling
-
 public class Gp2gpConfiguration {
     private int largeAttachmentThreshold;
     private int largeEhrExtractThreshold;
