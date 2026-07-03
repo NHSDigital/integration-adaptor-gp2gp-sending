0b93183633ab07eae565509cda9e1b5f1a9e3dfa
ORybak5 <12736698+ORybak5@users.noreply.github.com>
2026-07-03 13:19:06 +0100

    Add more log coverage (#1502)
    * add more test coverage
    
    * add more logs coverage
    
    * addressing pitest
    
    * addressing pitest
    
    * adding more logs coverage
    
    * addressing pitest

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/status/controller/EhrStatusRequestsController.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/status/controller/EhrStatusRequestsController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import uk.nhs.adaptors.gp2gp.ehr.status.model.EhrStatusRequest;
 import uk.nhs.adaptors.gp2gp.ehr.status.model.EhrStatusRequestQuery;
 import uk.nhs.adaptors.gp2gp.ehr.status.service.EhrStatusRequestsService;
 
 @RestController
+@Slf4j
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping(path = "/requests")
 public class EhrStatusRequestsController {
@@ -25,16 +27,42 @@ public class EhrStatusRequestsController {
 
     @PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<EhrStatusRequest>> getEhrRequestsEncodedForm(EhrStatusRequestQuery request) {
+        LOGGER.info("Received form-encoded request for EHR status requests");
+        try {
+            Optional<List<EhrStatusRequest>> ehrRequestOptional = ehrRequestsService.getEhrStatusRequests(request);
 
-        Optional<List<EhrStatusRequest>> ehrRequestOptional = ehrRequestsService.getEhrStatusRequests(request);
-        return ehrRequestOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+            if (ehrRequestOptional.isPresent()) {
+                List<EhrStatusRequest> requests = ehrRequestOptional.get();
+                LOGGER.info("Found {} EHR status requests", requests.size());
+                return ResponseEntity.ok(requests);
+            } else {
+                LOGGER.warn("No EHR status requests found");
+                return ResponseEntity.noContent().build();
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving EHR status requests (form-encoded)", e);
+            throw e;
+        }
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<EhrStatusRequest>> getEhrRequests(@RequestBody EhrStatusRequestQuery request) {
+        LOGGER.info("Received JSON request for EHR status requests");
+        try {
+            Optional<List<EhrStatusRequest>> ehrRequestOptional = ehrRequestsService.getEhrStatusRequests(request);
 
-        Optional<List<EhrStatusRequest>> ehrRequestOptional = ehrRequestsService.getEhrStatusRequests(request);
-        return ehrRequestOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+            if (ehrRequestOptional.isPresent()) {
+                List<EhrStatusRequest> requests = ehrRequestOptional.get();
+                LOGGER.info("Found {} EHR status requests", requests.size());
+                return ResponseEntity.ok(requests);
+            } else {
+                LOGGER.warn("No EHR status requests found");
+                return ResponseEntity.noContent().build();
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving EHR status requests (JSON)", e);
+            throw e;
+        }
     }
 
 }
