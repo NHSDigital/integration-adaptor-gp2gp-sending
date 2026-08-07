ad080d6b3e1585b9c0cb178586eacb8644409531
ORybak5 <12736698+ORybak5@users.noreply.github.com>
2026-08-07 14:32:49 +0100

    adding additional tests to cover log entry scenarios

--- service/src/test/java/uk/nhs/adaptors/gp2gp/common/amqp/JmsListenerErrorHandlerTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/amqp/JmsListenerErrorHandlerTest.java
@@ -1,10 +1,19 @@
 package uk.nhs.adaptors.gp2gp.common.amqp;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessResourceFailureException;
 
 import uk.nhs.adaptors.gp2gp.mhs.exception.MhsConnectionException;
@@ -12,11 +21,31 @@ import uk.nhs.adaptors.gp2gp.mhs.exception.MhsConnectionException;
 class JmsListenerErrorHandlerTest {
 
     private final JmsListenerErrorHandler jmsListenerErrorHandler = new JmsListenerErrorHandler();
+    private ListAppender<ILoggingEvent> logAppender;
+    private Logger logger;
+
+    @BeforeEach
+    void setUp() {
+        logger = (Logger) LoggerFactory.getLogger(JmsListenerErrorHandler.class);
+        logAppender = new ListAppender<>();
+        logAppender.start();
+        logger.addAppender(logAppender);
+    }
+
+    @AfterEach
+    void tearDown() {
+        logger.detachAppender(logAppender);
+        logAppender.stop();
+    }
 
     @Test
     void When_HandleError_WithNoCause_Expect_NoExceptionThrown() {
         assertThatCode(() -> jmsListenerErrorHandler.handleError(new RuntimeException("outer")))
             .doesNotThrowAnyException();
+
+        assertErrorContains("Handling JMS message error due to [java.lang.RuntimeException] with message [outer]");
+        assertThat(getErrorMessages())
+            .noneMatch(message -> message.contains("Caught Error cause of type"));
     }
 
     @Test
@@ -25,6 +54,9 @@ class JmsListenerErrorHandlerTest {
 
         assertThatCode(() -> jmsListenerErrorHandler.handleError(throwable))
             .doesNotThrowAnyException();
+
+        assertErrorContains("Handling JMS message error due to [java.lang.RuntimeException] with message [outer]");
+        assertErrorContains("Caught Error cause of type: [class java.lang.IllegalArgumentException], with message: [inner]");
     }
 
     @Test
@@ -34,6 +66,10 @@ class JmsListenerErrorHandlerTest {
         assertThatThrownBy(() -> jmsListenerErrorHandler.handleError(throwable))
             .isInstanceOf(RuntimeException.class)
             .hasMessage("Unable to access database");
+
+        assertErrorContains("Handling JMS message error due to [java.lang.RuntimeException] with message [outer]");
+        assertErrorContains("Caught Error cause of type: [class org.springframework.dao.DataAccessResourceFailureException], "
+            + "with message: [db down]");
     }
 
     @Test
@@ -43,5 +79,20 @@ class JmsListenerErrorHandlerTest {
         assertThatThrownBy(() -> jmsListenerErrorHandler.handleError(throwable))
             .isInstanceOf(RuntimeException.class)
             .hasMessage("Unable to connect to MHS Outbound");
+
+        assertErrorContains("Handling JMS message error due to [java.lang.RuntimeException] with message [outer]");
+        assertErrorContains("Caught Error cause of type: [class uk.nhs.adaptors.gp2gp.mhs.exception.MhsConnectionException], "
+            + "with message: [MHS down]");
+    }
+
+    private void assertErrorContains(String expectedMessagePart) {
+        assertThat(getErrorMessages()).anyMatch(message -> message.contains(expectedMessagePart));
+    }
+
+    private List<String> getErrorMessages() {
+        return logAppender.list.stream()
+            .filter(event -> event.getLevel() == Level.ERROR)
+            .map(ILoggingEvent::getFormattedMessage)
+            .toList();
     }
 }
